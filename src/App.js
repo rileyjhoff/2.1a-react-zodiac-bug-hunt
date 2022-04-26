9bce13813aaa319ca5c467b8bf5bee2b52de2fc6
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-26 14:28:39 -0700

    fixed file path to import the zodiac array from data.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Main from './components/Main/Main'
-import { zodiac } from '../../data.js'
+import { zodiac } from './data.js'
 
 function App() {
   return (
