ef09e7312cd2e0b278cfb6c2330e7f2622ebff98
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-26 14:27:46 -0700

    fixed adjacency issue in ZodiacCard component but removing the h2 element. Also fixed ZodiacCard.css import issue

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -1,8 +1,7 @@
-import './zodiac-card.css'
+import './ZodiacCard.css'
 
-export default function ZodiacCard(props) {
+export default function ZodiacCard({ name, dates }) {
   return (
-    <h2>Zodiac card</h2>
     <div className="zodiac-card">
       <img alt={name} src={`/images/${name}`} />
       <span className="name">{name}</span>
