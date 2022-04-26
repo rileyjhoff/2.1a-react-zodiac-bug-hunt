9ae4821d1821d5c58356185106f96be3e1c18cb6
Riley Hoffman <rileyjhoff@gmail.com>
2022-04-26 14:30:57 -0700

    fixed file path for images in the ZodiacCard component by including .png

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -3,7 +3,7 @@ import './ZodiacCard.css'
 export default function ZodiacCard({ name, dates }) {
   return (
     <div className="zodiac-card">
-      <img alt={name} src={`/images/${name}`} />
+      <img alt={name} src={`/images/${name}.png`} />
       <span className="name">{name}</span>
       <span>{dates}</span>
     </div>
